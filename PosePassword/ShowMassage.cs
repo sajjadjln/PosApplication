5a81164fccba690c8d2b0836e29feb67a3d690ec
sajjad jalilyan <sajjadjlm1999@gmail.com>
2022-07-30 10:48:13 +0430

    created show massage class for opening lines

---
+++ PosePassword/ShowMassage.cs
@@ -0,0 +1,50 @@
+namespace PosePassword
+{
+
+    public class ShowMassage
+    {
+        public Decimal Amount { get; set; }
+        public int DynamicPasswordSwitch()
+        {
+            Console.WriteLine("Welcome to password application");
+            Console.WriteLine("please enter the number of your desire option(1 or 2)\n");
+            Console.WriteLine("1.Create a new card\n2.choose a saved card and generate password\n3.edit a card information\n4.remove a card");
+            int FirstUserInput = 0;
+
+            try
+            {
+             FirstUserInput = Convert.ToInt32(Console.ReadLine());
+             Console.Clear();
+            }
+            catch (FormatException) //? how to escape the default massage from switch case. // throw or console.WriteLine
+            {
+            throw new FormatException("please enter a number");
+            //Console.Write("Please enter a number ");
+            }
+            return FirstUserInput;
+        }
+        public int PoseSwitch()
+        {
+            int FirstUserInput = 0;
+            Console.WriteLine("Welcome to Pose application\n\n");
+            Console.WriteLine("please Enter your desired amount");
+            var InputString = Console.ReadLine();
+            decimal Amount;
+            decimal.TryParse(InputString, out Amount);
+            this.Amount = Amount;
+            Console.Clear();
+            Console.WriteLine("1.using a saved card");
+            Console.WriteLine("2.entering card information");
+            try
+            {
+                FirstUserInput = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+            }catch (FormatException)
+            {
+                    throw new FormatException("please enter a number");
+            }
+            return FirstUserInput;
+        }
+    }
+
+}
