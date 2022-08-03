987d3c2aca5a8e6398994f8fc03d3aa1ccb875b1
sajjad jalilyan <sajjadjlm1999@gmail.com>
2022-08-03 17:09:30 +0430

    adding prompt clear

--- PosePassword/EditCard.cs
+++ PosePassword/EditCard.cs
@@ -18,16 +18,19 @@ namespace PosePassword
             Console.WriteLine($"your current card number is : {Cards[chosenId - 1].CardNumber}\n");
             Console.WriteLine("enter new card number : ");
             string? CardNumb = Console.ReadLine();
+            Console.Clear();
             Console.WriteLine($"your current cvv2 number is : {Cards[chosenId - 1].Cvv2}\n");
             Console.WriteLine("enter new cvv2 number : ");
             string? Cvv2Numb = Console.ReadLine();
+            Console.Clear();
             Console.WriteLine($"your current card expire month is : {Cards[chosenId - 1].DateMonth}\n");
             Console.WriteLine("enter new month date : ");
             string? MonthNumb = Console.ReadLine();
+            Console.Clear();
             Console.WriteLine($"your current card expire year is : {Cards[chosenId - 1].DateYear}\n");
             Console.WriteLine("enter new year date : ");
             string? YearNumb = Console.ReadLine();
-
+            Console.Clear();
              ConvertCardType convertedCardType = new ConvertCardType(
                 CardNumb,
                 Cvv2Numb,
